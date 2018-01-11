fba24e13a3040fe5e7e2855c2e78760a6c81aaa8
Zane D. R <26946175+TotallyAProgrammer@users.noreply.github.com>
2018-01-11 11:33:24 -0600

    Added the Controler

---
+++ Stepper-Control.ino
@@ -0,0 +1,17 @@
+#include <Stepper.h>
+
+#define STEPS 100 //You can changes this to what ever value you need.
+
+int potval = 0;
+
+Stepper stepper(STEPS, 8, 9, 10, 11); //This is what creates the link to the ports, 8, 9, 10, and 11
+
+void setup() {
+  stepper.setSpeed(30); //This sets the speed of the stepper
+}
+
+void loop() {
+  potval = analogRead(0); //This reads the potentiometer value
+  stepper.setSpeed(potval / 10); //This divides the potval by 10 so it can be used by the stepper
+  stepper.step(STEPS); //This tells the stepper to step by STEPS
+}
